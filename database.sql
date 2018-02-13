e39c8899a62931cc21657d6e8900f22a1c276aa9
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-02-13 16:50:15 +0100

    added fest tables to database.sql.

--- database.sql
+++ database.sql
@@ -16,6 +16,41 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `fest`
+--
+
+DROP TABLE IF EXISTS `fest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `place` varchar(255) NOT NULL,
+  `start` varchar(255) NOT NULL,
+  `end` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `ticket_count_d1` int(11) NOT NULL,
+  `ticket_count_d2` int(11) NOT NULL,
+  `ticket_count_d3` int(11) NOT NULL,
+  `ticket_price_d1` int(11) NOT NULL,
+  `ticket_price_d2` int(11) NOT NULL,
+  `ticket_price_d3` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fest`
+--
+
+LOCK TABLES `fest` WRITE;
+/*!40000 ALTER TABLE `fest` DISABLE KEYS */;
+INSERT INTO `fest` VALUES (1,'FestiCoquin','Dans ta chambre','13/02/2018','14/02/2018','Il parait que la nuit, tout est permis...',2,2,0,69,69,0),(2,'Dark Souls Fest','Dans tes cauchemards','19/02/2018','21/02/2018','C\'est hardcore et tu vas prendre cher...',50,50,50,30,30,30),(3,'LoveFest','Dans tes rêves','29/03/2018','29/03/2018','I love you, you love me... Le festival des amoureux.ses!',200,250,200,10,12,10);
+/*!40000 ALTER TABLE `fest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `members`
 --
 
@@ -50,4 +85,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-13 15:55:14
+-- Dump completed on 2018-02-13 16:49:59
