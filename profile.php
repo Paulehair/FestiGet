2ec30bf8728e3a0fc6cb2924e493ed1b7ef86f48
Paule Herman <hermanpaule@gmail.com>
2018-02-15 17:15:34 +0100

    profile style

--- profile.php
+++ profile.php
@@ -28,21 +28,42 @@ if (isset($_GET['id']) && $_GET['id'] > 0) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/font-face.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/profile.css">
     <title>Profile</title>
 </head>
 <body>
+<div class="header">
+    <div class="logo">
+        <p class="festiget">FESTI<span class="pink">GET</span></p>
+        <p class="logoText">get to your festival</p>
+    </div>
+    <div class="headerItems">
+        <div class="search">
+            <input type="text" class="searchBar" placeholder="Search...">
+        </div>
+        <div class="card">
+            <a href=""><img class="cardImg" src="assets/img/cartcadi.png" alt="card" title="card"></a>
+        </div>
+        <div class="profile">
+            <a href=""><img class="ppImg" src="assets/img/pp.png" alt="profile" title="profile picture"></a>
+        </div>
+    </div>
+</div>
 <div>
-    <h2>Welcome <?=$row['pseudo']?>!</h2>
+    <h2 class="title">Welcome <?=$row['pseudo']?>!</h2>
 
-    <p>Pseudo <?=$row['pseudo']?></p>
-    <p>Mail <?=$row['mail']?></p>
+    <p class = "name">Pseudo <?=$row['pseudo']?></p>
+    <p class="mail">Mail <?=$row['mail']?></p>
     <?php
         //check if 'id' exists = someone is connected AND if id of session equals id of page
         if (isset($_SESSION['id']) && $row['id'] == $_SESSION['id']) {
             ?>
-            <a class="linkEdit" href="edit.php">Edit profile</a>
+            <a class="link" href="edit.php">Edit profile</a>
             </br>
-            <a href="deconnect.php">Deconnect</a>
+            <a class="link" href="deconnect.php">Deconnect</a>
             <?php
         } else {
             echo "lol";
